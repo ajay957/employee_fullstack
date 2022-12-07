69f0d562405800cecc1372579f71a4dd977835b8
Ajay957 <ajay74.aa82@gmail.com>
2022-12-07 08:25:42 +0530

    fourth commit

---
+++ src/app/view-employee/view-employee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-employee',
+  templateUrl: './view-employee.component.html',
+  styleUrls: ['./view-employee.component.css']
+})
+export class ViewEmployeeComponent {
+
+  data:any=[]
+}
